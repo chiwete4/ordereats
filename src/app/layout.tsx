51c3333c21a126eb24b3590c4dc28387b5faa2f4
Chiwete uchemefuna <80927742+chiwete4@users.noreply.github.com>
2026-09-22 08:03:07 +0100

    Load Mapbox map styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { ToastProvider } from "@/components/toast-provider";
 
-import "./globals.css";
+import "mapbox-gl/dist/mapbox-gl.css";\nimport "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
